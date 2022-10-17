afdd0e6780d99a010938fa75f65a37a1c72b121d
mobisa-012 <mobisakwamboka@gmail.com>
2022-10-17 17:46:06 +0300

    const path and text

---
+++ hosy/lib/core/const/path_constants.dart
@@ -0,0 +1,5 @@
+class PathConstants {
+  static const String ambulance = 'assets/images/Ambulance.png';
+  static const String heart = 'assets/images/heart.png';
+  static const String foot = 'assets/images/foot.png';
+}
---
+++ hosy/lib/core/const/text_constants.dart
@@ -0,0 +1,32 @@
+class TextConstants {
+  //onboarding
+  static const String hosy = 'Hosy';
+  static const String needAmbulance = 'Need an ambulance?';
+  static const String phoneNumber =
+      'Enter phone number to book or order an ambulance';
+  static const String otp = 'Send OTP';
+  static const String enterOTP = 'Enter OTP';
+  static const String verify = 'Verify';
+  static const String alwaysReady = 'Always ready, anytime, anywhere';
+
+  //Home screen
+  static const String order = 'Order';
+  static const String book = 'Book';
+  static const String orderAmbulance = 'Order Ambulance';
+  static const String firstAid = 'First Aid';
+
+  //Hospitals
+  static const String hospitals = 'Hospitals';
+  static const String findHospital = 'find a hospital';
+
+  //Doctors
+  static const String findDoctor = 'find a doctor';
+  static const String doctor = 'Let\'s find you a doctor';
+  static const String setDate = 'Set date to see the doctor';
+
+  //Profile
+  static const String inviteFriends = 'Invite friends';
+  static const String history = 'Hostory';
+  static const String help = 'Help';
+  static const String paymentMethod = 'Payment method';
+}
