1c1216cb47c668b932fa494e77af1401f12ba418
mobisa-012 <mobisakwamboka@gmail.com>
2022-10-17 21:39:19 +0300

    changed image

--- hosy/lib/screens/common_widgets/profile_container.dart
+++ hosy/lib/screens/common_widgets/profile_container.dart
@@ -1,21 +1,18 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:hosy/core/const/colors.dart';
 
 class ProfileContainer extends StatelessWidget {
-  final bool withCall;
   final Widget child;
   final Function()? onTap;
   
-  const ProfileContainer({super.key, required this.child, this.onTap, required this.withCall});
+  const ProfileContainer({super.key, required this.child, this.onTap, });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10),
       child: Container(
+        padding: const EdgeInsets.all(8),
         width: double.infinity,
         height: 50,
         decoration: BoxDecoration(
@@ -37,16 +34,7 @@ class ProfileContainer extends StatelessWidget {
               child: Row(
                 children: [
                   Expanded(child: child),
-                  if(withCall) const Icon(
-                    Icons.add_call,
-                    color: AppColors.buttonColor,
-                    size: 20,
-                  ),
-                  const Icon(
-                    Icons.info,
-                  color: AppColors.buttonColor,
-                  size: 20,)
-                ],
+                  ],
               ),
             ),
           ),
