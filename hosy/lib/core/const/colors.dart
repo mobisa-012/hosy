a0d19226bbb9e0fbddc7e7e9f37d86dbe3301637
mobisa-012 <mobisakwamboka@gmail.com>
2022-10-17 17:55:51 +0300

    colors file

---
+++ hosy/lib/core/const/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const homeBackground = Color(0XFFffffff);
+  static const textColor = Color.fromARGB(255, 8, 1, 20);
+  static const buttonTextColor = Color(0XFFffffff);
+  static const buttonColor = Color.fromARGB(255, 1, 1, 17);
+  static const profiletextColor = Color.fromARGB(255, 8, 1, 20);
+}
