221533095cc1fc7810969b5c30cdbe84a38359b6
mobisa-012 <mobisakwamboka@gmail.com>
2022-10-17 21:38:01 +0300

    edit account scrren logic

---
+++ hosy/lib/screens/edit_account/page/edit_account.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class EditAccountScreen extends StatefulWidget {
+  const EditAccountScreen({super.key});
+
+  @override
+  State<EditAccountScreen> createState() => _EditAccountScreenState();
+}
+
+class _EditAccountScreenState extends State<EditAccountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
