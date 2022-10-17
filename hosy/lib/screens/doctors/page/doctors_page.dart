d8c1adf7aaf01b37350f577d38e8d39f0949bea8
mobisa-012 <mobisakwamboka@gmail.com>
2022-10-17 19:50:54 +0300

    removed import

---
+++ hosy/lib/screens/doctors/page/doctors_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Doctors extends StatefulWidget {
+  const Doctors({super.key});
+
+  @override
+  State<Doctors> createState() => _DoctorsState();
+}
+
+class _DoctorsState extends State<Doctors> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ hosy/lib/screens/hospitals/page/hospotals_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Hospitals extends StatefulWidget {
+  const Hospitals({super.key});
+
+  @override
+  State<Hospitals> createState() => _HospitalsState();
+}
+
+class _HospitalsState extends State<Hospitals> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
