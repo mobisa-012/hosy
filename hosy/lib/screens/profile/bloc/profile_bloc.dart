0bbb7e0c6589cbf7f0fd1084317e2759856fab38
mobisa-012 <mobisakwamboka@gmail.com>
2022-10-17 18:57:45 +0300

    profile page logic

---
+++ hosy/lib/screens/profile/bloc/profile_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'profile_state.dart';
+part 'profile_event.dart';
+
+class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
+  ProfileBloc() : super(ProfileInitial());
+
+  Stream<ProfileState> mapEventToState(
+    ProfileEvent event
+  ) async* {}
+}
