aed6eab9686393caa0cd0b84eca78c274d389047
mobisa-012 <mobisakwamboka@gmail.com>
2022-10-18 22:52:56 +0300

    hospital page

--- hosy/lib/screens/hospitals/page/hospotals_page.dart
+++ hosy/lib/screens/hospitals/page/hospotals_page.dart
@@ -1,16 +1,31 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hosy/screens/hospitals/bloc/hospital_bloc.dart';
+import 'package:hosy/screens/hospitals/widgets/hospital_content.dart';
 
-class Hospitals extends StatefulWidget {
+class Hospitals extends StatelessWidget {
   const Hospitals({super.key});
 
   @override
-  State<Hospitals> createState() => _HospitalsState();
-}
-
-class _HospitalsState extends State<Hospitals> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: _buildContext(context),
+    );
+  }
+
+  BlocProvider<HospitalBloc> _buildContext(BuildContext context) {
+    return BlocProvider<HospitalBloc>(
+      create: (BuildContext context) => HospitalBloc(),
+      child: BlocConsumer<HospitalBloc, HospitalState>(
+        buildWhen: (_, currState) => currState is HospitalInitial,
+        builder: (context, state) {
+          return const HospitalContent();
+        },
+        listenWhen: (_, currState) => true,
+        listener: (context, state) {},
+      ),
+    );
   }
 }
